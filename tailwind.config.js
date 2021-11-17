c601a2f7538d3601219fc6ea3c9611f1b63537e1
adnan614 <adnanfaruque23@gmail.com>
2021-11-18 00:43:24 +0600

    tab in my account

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkred: {
+          50: "#00B5E2",
+          100: "#0EA0C5",
+          200: "#C83637",
+          300: "#EE4054",
+          400: "#EE4552",
+        },
+        mygray: {
+          50: "#F5F5F5",
+          100: "#F5F5F5",
+          200: "#F3F6FA",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
